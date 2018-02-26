8100f480744083e419e265a6f3c8a52222cdf4c5
Benjamí Borràs <benjami.borras@gmail.com>
2018-02-27 00:04:34 +0100

    Modified some files to fix template issues and code structure

--- src/app/code/community/Thirty4/CatalogSale/Block/Widget/Carousel.php
+++ src/app/code/community/Thirty4/CatalogSale/Block/Widget/Carousel.php
@@ -1,15 +1,18 @@
 <?php
 
-class Thirty4_CatalogSale_Block_Widget_Carousel
-  extends Thirty4_CatalogSale_Block_Carousel
-  implements Mage_Widget_Block_Interface
+class Thirty4_CatalogSale_Block_Widget_Carousel extends Thirty4_CatalogSale_Block_Carousel 
+    implements Mage_Widget_Block_Interface
 {
-  public function getProductsCount()
-  {
-    if (!$this->hasData('products_count')) {
-      return parent::getProductsCount();
+    /**
+     * Get products count
+     * 
+     * @return integer
+     */
+    public function getProductsCount()
+    {
+        if (!$this->hasData('products_count')) {
+            return parent::getProductsCount();
+        }
+        return $this->_getData('products_count');
     }
-    return $this->_getData('products_count');
-  }
-  
 }
