942d873b0ffdc6ef97885fcfce4c2c3da78892b3
Benjamí Borràs <benjami.borras@gmail.com>
2018-04-22 21:44:31 +0200

    Created Losreservas1811_Sales module to add company and CIF/NIF/NIE fields in address management

---
+++ src/app/code/local/Losreservas1811/Sales/Helper/Data.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Magento e-commerce
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ *
+ * @category   Losreservas1811
+ * @package    Losreservas1811_Sales
+ * @copyright  Copyright (c) 2018 LOS RESERVAS 1811, SL (https://www.losreservas1811.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+/**
+ * Sales Data helper
+ *
+ * @category   Losreservas1811
+ * @package    Losreservas1811_Sales
+ * @author     LOS RESERVAS 1811 Team <web@losreservas1811.com>
+ */
+class Losreservas1811_Sales_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
